84c645d68f8024b67da916ae7151bad512c6da90
Mirai3103 <huuhoag1412@gmail.com>
2024-03-05 19:11:43 +0700

    update requirements.txt

--- fastapi-server/app/entities/users.py
+++ fastapi-server/app/entities/users.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from app.database.database import Base, engine
+from sqlalchemy import Boolean, Column, Integer, String
+from app.database.database import Base
 
 
 class User(Base):
