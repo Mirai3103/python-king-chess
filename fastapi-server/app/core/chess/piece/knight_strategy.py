b1b724002f2210f6a921e951c056c34cb1a76c74
Mirai3103 <huuhoag1412@gmail.com>
2024-03-15 18:57:43 +0700

    #3 Implement KnightStrategy class and add unit tests for knight moves

--- fastapi-server/app/core/chess/piece/knight_strategy.py
+++ fastapi-server/app/core/chess/piece/knight_strategy.py
@@ -1,11 +1,40 @@
 
 
+from typing import Optional
 from app.core.chess.chess import Chess
-from ..type import InternalMove
+from ..type import CellName, InternalMove, MoveType, PieceType
 from .picece_strategy import PieceStrategy
 
 class KnightStrategy(PieceStrategy):
-    def get_moves(self, game: Chess, from_2d:tuple[int,int]) -> list[InternalMove]:
-        pass
-    def check_move(self, game: Chess, from_2d:tuple[int,int], to_2d:tuple[int,int]) -> InternalMove:
-        pass
+    def get_moves(self, game: Chess, from_2d: tuple[int, int]) -> list[InternalMove]:
+        board = game._board
+        fromX, fromY = from_2d
+        piece = board[fromX][fromY]
+        moves = []
+        # Các hướng di chuyển của quân mã
+        directions = [
+            (-2, -1), (-1, -2), (1, -2), (2, -1),
+            (2, 1), (1, 2), (-1, 2), (-2, 1)
+        ]
+        for dx, dy in directions:
+            toX, toY = fromX + dx, fromY + dy
+
+            # Kiểm tra xem ô đích có nằm trong bàn cờ và không phải là quân cờ cùng màu
+            if 0 <= toX < 8 and 0 <= toY < 8 and (board[toX][toY].pieceType == PieceType.EMPTY or board[toX][toY].pieceColor != piece.pieceColor):
+                captured_piece = board[toX][toY]
+                move_type = MoveType.CAPTURE if captured_piece.pieceType != PieceType.EMPTY else MoveType.NORMAL
+
+                move = InternalMove(CellName.from_2d(fromX, fromY), CellName.from_2d(toX, toY), PieceType.KNIGHT, captured_piece, piece, move_type)
+
+                # Kiểm tra xem nước đi có gây ra tình trạng "chiếu tướng" hay không
+                if not game.simulate_move(move).is_check(piece.pieceColor):
+                    moves.append(move)
+
+        return moves
+
+    def check_move(self, game: Chess, from_2d: tuple[int, int], to_2d: tuple[int, int]) -> Optional[InternalMove]:
+        valid_moves = self.get_moves(game, from_2d)
+        for move in valid_moves:
+            if move._to == CellName.from_2d(*to_2d):
+                return move
+        return None
