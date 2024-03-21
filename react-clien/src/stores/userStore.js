2faf14e0bc311a3c9f6a5f2f4918cffb582cf25b
Mirai3103 <huuhoag1412@gmail.com>
2024-03-21 22:26:13 +0700

    Add persistence to userStore using Zustand middleware #11

--- react-clien/src/stores/userStore.js
+++ react-clien/src/stores/userStore.js
@@ -1,10 +1,13 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 
-
-export const useUserStore = create((set) => ({
+export const useUserStore = create(persist((set) => ({
   user_id: null,
   display_name: null,
   setUserId: (id) => set({ user_id: id }),
   setDisplayName: (name) => set({ display_name: name }),
+}), {
+  name: 'user-storage',
+  storage:createJSONStorage(()=>localStorage)
 }));
