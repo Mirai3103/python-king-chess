7a7b65e57cf7893e7ac8b4f102abb229f2d2a66d
Văn <95088369+maivantai2003@users.noreply.github.com>
2024-03-25 02:14:11 +0700

    update king_strategy

--- fastapi-server/app/core/chess/piece/knight_strategy.py
+++ fastapi-server/app/core/chess/piece/knight_strategy.py
@@ -4,6 +4,39 @@ from typing import Optional
 from ..type import CellName, IChess, InternalMove, MoveType, PieceType
 from .picece_strategy import PieceStrategy
 
+# class KnightStrategy(PieceStrategy):
+#     def get_moves(self, game: IChess, from_2d: tuple[int, int]) -> list[InternalMove]:
+#         board = game._board
+#         fromX, fromY = from_2d
+#         piece = board[fromX][fromY]
+#         moves = []
+#         # Các hướng di chuyển của quân mã
+#         directions = [
+#             (-2, -1), (-1, -2), (1, -2), (2, -1),
+#             (2, 1), (1, 2), (-1, 2), (-2, 1)
+#         ]
+#         for dx, dy in directions:
+#             toX, toY = fromX + dx, fromY + dy
+
+#             # Kiểm tra xem ô đích có nằm trong bàn cờ và không phải là quân cờ cùng màu
+#             if 0 <= toX < 8 and 0 <= toY < 8 and (board[toX][toY].pieceType == PieceType.EMPTY or board[toX][toY].pieceColor != piece.pieceColor):
+#                 captured_piece = board[toX][toY]
+#                 move_type = MoveType.CAPTURE if captured_piece.pieceType != PieceType.EMPTY else MoveType.NORMAL
+
+#                 move = InternalMove(CellName.from_2d(fromX, fromY), CellName.from_2d(toX, toY), PieceType.KNIGHT, captured_piece, piece, move_type)
+
+#                 # Kiểm tra xem nước đi có gây ra tình trạng "chiếu tướng" hay không
+#                 if not game.simulate_move(move).is_check(piece.pieceColor):
+#                     moves.append(move)
+
+#         return moves
+
+#     def check_move(self, game: IChess, from_2d: tuple[int, int], to_2d: tuple[int, int]) -> Optional[InternalMove]:
+#         valid_moves = self.get_moves(game, from_2d)
+#         for move in valid_moves:
+#             if move._to == CellName.from_2d(*to_2d):
+#                 return move
+#         return None
 class KnightStrategy(PieceStrategy):
     def get_moves(self, game: IChess, from_2d: tuple[int, int]) -> list[InternalMove]:
         board = game._board
@@ -18,16 +51,18 @@ class KnightStrategy(PieceStrategy):
         for dx, dy in directions:
             toX, toY = fromX + dx, fromY + dy
 
-            # Kiểm tra xem ô đích có nằm trong bàn cờ và không phải là quân cờ cùng màu
-            if 0 <= toX < 8 and 0 <= toY < 8 and (board[toX][toY].pieceType == PieceType.EMPTY or board[toX][toY].pieceColor != piece.pieceColor):
-                captured_piece = board[toX][toY]
-                move_type = MoveType.CAPTURE if captured_piece.pieceType != PieceType.EMPTY else MoveType.NORMAL
+            # Kiểm tra xem ô đích có nằm trong bàn cờ không
+            if 0 <= toX < 8 and 0 <= toY < 8:
+                # Kiểm tra xem ô đích có chứa quân cờ và là quân đối phương hay không
+                if board[toX][toY].pieceType == PieceType.EMPTY or board[toX][toY].pieceColor != piece.pieceColor:
+                    captured_piece = board[toX][toY]
+                    move_type = MoveType.CAPTURE if captured_piece.pieceType != PieceType.EMPTY else MoveType.NORMAL
 
-                move = InternalMove(CellName.from_2d(fromX, fromY), CellName.from_2d(toX, toY), PieceType.KNIGHT, captured_piece, piece, move_type)
+                    move = InternalMove(CellName.from_2d(fromX, fromY), CellName.from_2d(toX, toY), PieceType.KNIGHT, captured_piece, piece, move_type)
 
-                # Kiểm tra xem nước đi có gây ra tình trạng "chiếu tướng" hay không
-                if not game.simulate_move(move).is_check(piece.pieceColor):
-                    moves.append(move)
+                    # Kiểm tra xem nước đi có gây ra tình trạng "chiếu tướng" hay không
+                    if not game.simulate_move(move).is_check(piece.pieceColor):
+                        moves.append(move)
 
         return moves
 
