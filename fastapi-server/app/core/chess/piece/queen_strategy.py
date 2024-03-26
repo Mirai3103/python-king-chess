ff614de24c2f9544f10d4737fa82130f6760fbf6
Mirai3103 <huuhoag1412@gmail.com>
2024-03-26 17:03:58 +0700

    Refactor queen_strategy.py: Update get_piece_color arguments #5

--- fastapi-server/app/core/chess/piece/queen_strategy.py
+++ fastapi-server/app/core/chess/piece/queen_strategy.py
@@ -20,7 +20,7 @@
 #     def get_moves(self, game: IChess, from_2d: tuple[int, int]) -> list[InternalMove]:
 #         moves = []
 #         x, y = from_2d
-#         color = game.get_piece_color(CellName.from_2d(from_2d))
+#         color = game.get_piece_color(CellName.from_2d(fromX, fromY))
 #         directions = [
 #             (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
 #         ]  # Tất cả các hướng có thể di chuyển
@@ -42,7 +42,7 @@
 #     def check_move(self, game: IChess, from_2d: tuple[int, int], to_2d: tuple[int, int]) -> Optional[InternalMove]:
 #         x, y = from_2d
 #         to_x, to_y = to_2d
-#         color = game.get_piece_color(CellName.from_2d(from_2d))
+#         color = game.get_piece_color(CellName.from_2d(fromX, fromY))
 #         dx = to_x - x
 #         dy = to_y - y
 
@@ -57,7 +57,7 @@
 #             if game.get_piece_color(CellName.from_2d(to_2d)) == color:
 #                 return None  # Ô đích có quân cùng màu
 #             move_type = MoveType.CAPTURE if game.get_piece_type(CellName.from_2d(to_2d)) != PieceType.EMPTY else MoveType.NORMAL
-#             return InternalMove(CellName.from_2d(from_2d), CellName.from_2d(to_2d), move_type)
+#             return InternalMove(CellName.from_2d(fromX, fromY), CellName.from_2d(to_2d), move_type)
 
 #         # Kiểm tra theo đường chéo
 #         if abs(dx) == abs(dy):
@@ -70,7 +70,7 @@
 #             if game.get_piece_color(CellName.from_2d(to_2d)) == color:
 #                 return None  # Ô đích có quân cùng màu
 #             move_type = MoveType.CAPTURE if game.get_piece_type(CellName.from_2d(to_2d)) != PieceType.EMPTY else MoveType.NORMAL
-#             return InternalMove(CellName.from_2d(from_2d), CellName.from_2d(to_2d), move_type)
+#             return InternalMove(CellName.from_2d(fromX, fromY), CellName.from_2d(to_2d), move_type)
 
 #         return None  # Không di chuyển theo đường ngang, dọc hoặc chéo
 from typing import Optional
@@ -81,7 +81,8 @@ class QueenStrategy(PieceStrategy):
     def get_moves(self, game: IChess, from_2d: tuple[int, int]) -> list[InternalMove]:
         moves = []
         fromX, fromY = from_2d
-        color = game.get_piece_color(CellName.from_2d(from_2d))
+        print(fromX, fromY)
+        color = game.get_piece_color(CellName.from_2d(fromX, fromY))
         piece = game._board[fromX][fromY]
         directions = [
             (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
@@ -105,7 +106,7 @@ class QueenStrategy(PieceStrategy):
     def check_move(self, game: IChess, from_2d: tuple[int, int], to_2d: tuple[int, int]) -> Optional[InternalMove]:
         fromX, fromY = from_2d
         toX, toY = to_2d
-        color = game.get_piece_color(CellName.from_2d(from_2d))
+        color = game.get_piece_color(CellName.from_2d(fromX, fromY))
         piece = game._board[fromX][fromY]
         dx = toX - fromX
         dy = toY - fromY
