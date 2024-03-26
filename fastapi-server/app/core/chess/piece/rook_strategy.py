eea7cfe4fbf98b0d95cb28ce21a0dca4071c4b1d
Mirai3103 <huuhoag1412@gmail.com>
2024-03-26 17:04:14 +0700

    Fix incorrect variable names in rook_strategy.py #6

--- fastapi-server/app/core/chess/piece/rook_strategy.py
+++ fastapi-server/app/core/chess/piece/rook_strategy.py
@@ -22,7 +22,7 @@
 #     def get_moves(self, game: IChess, from_2d: tuple[int, int]) -> list[InternalMove]:
 #         moves = []
 #         x, y = from_2d
-#         color = game.get_piece_color(CellName.from_2d(from_2d))
+#         color = game.get_piece_color(CellName.from_2d(fromX, fromY))
 #         directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]  # Các hướng quân xe có thể di chuyển
 
 #         for dx, dy in directions:
@@ -42,7 +42,7 @@
 #     def check_move(self, game: IChess, from_2d: tuple[int, int], to_2d: tuple[int, int]) -> Optional[InternalMove]:
 #         x, y = from_2d
 #         to_x, to_y = to_2d
-#         color = game.get_piece_color(CellName.from_2d(from_2d))
+#         color = game.get_piece_color(CellName.from_2d(fromX, fromY))
 #         dx = to_x - x
 #         dy = to_y - y
 
@@ -57,7 +57,7 @@
 #             if game.get_piece_color(CellName.from_2d(to_2d)) == color:
 #                 return None  # Ô đích có quân cùng màu
 #             move_type = MoveType.CAPTURE if game.get_piece_type(CellName.from_2d(to_2d)) != PieceType.EMPTY else MoveType.NORMAL
-#             return InternalMove(CellName.from_2d(from_2d), CellName.from_2d(to_2d), move_type)
+#             return InternalMove(CellName.from_2d(fromX, fromY), CellName.from_2d(to_2d), move_type)
 
 #         return None  # Không di chuyển theo đường ngang hoặc dọc
 from typing import Optional
